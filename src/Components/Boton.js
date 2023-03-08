196515e735535ee95cd6d6854dc9a08b2942ddbf
OmyLara <omy.lara.03@gmail.com>
2023-03-07 19:50:09 -0600

    Empezando

---
+++ src/Components/Boton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+import "./Boton.css";
+
+export default class Boton extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return (
+            <button>Hello</button>
+
+
+        )
+    }
+}
