5a393b42a5bfcf123f1167f9b44dd8eadae98334
OmyLara <omy.lara.03@gmail.com>
2023-10-17 14:19:10 -0600

    tercer commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,13 +2,15 @@ import React from "react";
 import './Contact.css';
 import github from '../Imagenes/github.png';
 import whats from '../Imagenes/whatsapp.png';
+import linkedin from '../Imagenes/linkedin.png'
 
 
 
 export default function Contact(){
     return(
-<>    
-    <form className="contactame">
+<div className="un">   
+{/*
+<form className="contactame">
         <label htmlFor='name'>Name</label>
         <input id="name" type="text" placeholder="Type your name"/><br/><br/>
 
@@ -21,11 +23,15 @@ export default function Contact(){
         <button>Send</button><br/>
         
     </form>
+*/} 
+    
     <div className="icons">
-    <a href="https://wa.me/525951108949/?text=Envía%20un%20mensaje%20a%20Omar%20Lara" target="_blank" rel="noreferrer"><img alt="WA" src={whats} className="ima-1"/></a>
-        <img src={github} alt="Github" className="ima-2"/>
+    <a href="https://wa.me/525951108949/?text=Envía%20un%20mensaje%20a%20Omar%20Lara" target="_blank" rel="noreferrer"><span>What's App</span><br/><br/><img alt="WA" src={whats} className="ima-1"/><br/></a><br/><br/><br/><br/>
+    <a href="https://github.com/OmyLara" target="_blank" rel="noreferrer"><span>Github</span><br/><br/><img src={github} alt="Github" className="ima-2"/><br/></a><br/><br/><br/><br/>
+    <a href="https://www.linkedin.com/in/lara-omar" target="_blank" rel="noreferrer"><span>LinkedIn</span><br/><br/><img src={linkedin} alt="Linkedin" className="ima-3"/><br/><br/></a>
+
     </div>
-</>
+</div>
 
         
     )
--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -7,13 +7,11 @@ export default function Experience(){
 
     const [color,setColor]=useState('#a317d2');
     const [show,setShow] = useState(false);
-    const [show2,setShow2] = useState(false);
-
     
         useEffect(()=>{
             function onMouseMove(evento){
                 if(evento.clientX < window.innerWidth / 2){
-                    setColor('rgb(76, 233, 212)');
+                    setColor('rgb(81, 191, 145)');
                 }else{
                     setColor('#a317d2');
                 }
@@ -25,57 +23,49 @@ export default function Experience(){
         const showing = () =>{
             setShow(!show);
         }
-
-        const showing2 = () =>{
-            setShow2(!show2);
-        }
-        
-        const lista = ['Data analysis for new developments in Latin America',
-        'Make sure features are released quickly, hassle-free and with quality',
-        'Spread knowledge of features for our internal clients',
-        'Identify and report bugs from our product to global product teams',
-        'Management of product projects launched globally'];
-        const listItems = lista.map(cosa => <li>{cosa}</li> );
-
-        const lista2 = ['Customer Support Mexico & LATAM at Doctoralia','Customer Success Senior at Doctoralia','Customer Experience Senior at Cabify'];
-        const listItems2 = lista2.map(cosa => <li>{cosa}</li> );
-
         const tab = '\u00A0';
+
+        
+        const lista = ['Product Expert Senior Mx & LATAM',
+        'Customer Support Mexico & LATAM',
+        'Customer Success Specialist',
+        'Customer Experience Senior'];
+        const listItems = lista.map(cosa => <li key={cosa}>{cosa}</li> );
+
+        
         
     return(
-        <div className="main">
             
-            <div className="circulo"></div>
-            <div className="linea"></div>
-            
+            <>
             <div className="fondo" style={{background: color}}>
+
                <div className="programming">
-               <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}C{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}H</p><br/>
-               <p><span className="r">J</span>{tab}<span className="o">A</span>{tab}<span className="o">V</span>{tab}<span className="o">A</span>{tab}<span className="r">S</span>{tab}<span className="o">C</span>{tab}<span className="r">R</span>{tab}<span className="o">I{tab}P</span>{tab}<span className="r">T</span></p><br/>
-               <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}S{tab}{tab}{tab}{tab}{tab}E{tab}{tab}{tab}{tab}{tab}{tab}M</p><br/>
-               <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}A{tab}{tab}{tab}{tab}{tab}{tab}L</p><br/>
-               <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}C</p><br/>
-               <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}T</p><br/>
+                <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}C{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}H</p><br/>
+                <p><span className="r">J</span>{tab}<span className="o">A</span>{tab}<span className="o">V</span>{tab}<span className="o">A</span>{tab}<span className="r">S</span>{tab}<span className="o">C</span>{tab}<span className="r">R</span>{tab}<span className="o">I{tab}P</span>{tab}<span className="r">T</span></p><br/>
+                <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}S{tab}{tab}{tab}{tab}{tab}E{tab}{tab}{tab}{tab}{tab}{tab}M</p><br/>
+                <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}A{tab}{tab}{tab}{tab}{tab}{tab}L</p><br/>
+                <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}C</p><br/>
+                <p className="o">{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}T</p><br/>
                </div>
 
-
                 <div className="DA">
-                    <h2 className="titulo">Product Expert Senior at Doctoralia</h2><br/>
+                    <h2 className="titulo">Previous Positions</h2><br/>
                     <img src={flecha} onClick={showing}></img><br/>
                     {/* esto lo desbloqueamos solo si queremos un botón que cambie la leyenda y no un icono <button onClick={showing}>{show ? 'Ocultar': 'Mostrar'}</button><br/>*/}
                     <ul style={{display: show ? 'block': 'none'}}>{listItems}</ul>
                 </div>
+                <footer>
+                    <a href="https://drive.google.com/drive/folders/1PT6Kvuoa6uPW3uKzlpijKcf8ifyFYV6e?usp=sharing" target="_blank" rel="noreferrer"><p>More info</p></a>
+                </footer>
+                
+                
                 
 
-                <div className="DA-2">
-                    <h2 className="titulo">More Jobs</h2><br/>
-                    <img src={flecha} onClick={showing2}></img><br/>
-                    <ul style={{display: show2 ? 'block': 'none'}}>{listItems2}</ul>
-                </div>
                 
-
+                
+                
             </div>
-        </div>
+            </>
        
     )
 }
